5a5db6cb640a50557f0ec3b6c815c3183463024d
GChris91 <GChris419@Gmail.com>
2018-04-30 16:01:04 +1100

    First Commmit. Has Interface but no drawing. Confirmation box almost ok.

--- src/sample/ConfirmBox.java
+++ src/sample/ConfirmBox.java
@@ -1,8 +1,12 @@
 package sample;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -11,29 +15,39 @@ public class ConfirmBox {
     static boolean confirmation;
 
     public static boolean display(String title, String message){
-        Stage window = new Stage();
-        window.initModality(Modality.APPLICATION_MODAL);
-        window.setTitle(title);
-        window.setMinWidth(250);
+        Stage primaryStage = new Stage();
+        primaryStage.initModality(Modality.APPLICATION_MODAL);
+        primaryStage.setTitle(title);
+        primaryStage.setMinWidth(250);
+
+        HBox layout = new HBox();
+        layout.setSpacing(10);
+
         Label label = new Label();
         label.setText(message);
 
         Button yesButton = new Button("Oui");
+        yesButton.setStyle("-fx-font-size: 10pt;");
         Button noButton = new Button("Non");
+        noButton.setStyle("-fx-font-size: 10pt;");
 
         yesButton.setOnAction(e -> {
             confirmation = true;
-            window.close();
+            primaryStage.close();
         });
 
         noButton.setOnAction(e -> {
             confirmation = false;
-            window.close();
+            primaryStage.close();
         });
-        Scene scene = new Scene();
-        window.setScene(scene);
-        window.showAndWait();
 
 
+        layout.getChildren().addAll(label, yesButton, noButton);
+        layout.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(layout);
+        primaryStage.setScene(scene);
+        primaryStage.showAndWait();
+
+        return confirmation;
     }
 }
